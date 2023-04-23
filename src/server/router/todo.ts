28e1f247b759c2c2323b1532eb52591d9a8ed0ed
OleksandrKovalskyi <modernxpunk@gmail.com>
2023-04-23 14:33:40 +0300

    add example of handling errors

--- src/server/router/todo.ts
+++ src/server/router/todo.ts
@@ -2,6 +2,7 @@ import { Todo } from "@prisma/client";
 import { publicProcedure, router } from "../trpc";
 import prisma from "../prisma";
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 
 const todoRouter = router({
 	getAll: publicProcedure.query(async () => {
@@ -17,6 +18,12 @@ const todoRouter = router({
 	}),
 	create: publicProcedure.input(z.string()).mutation(async (req) => {
 		const name = req.input;
+		if (name.length < 10) {
+			throw new TRPCError({
+				code: "BAD_REQUEST",
+				message: "Todo name must be at least 10 characters",
+			});
+		}
 		const todo: Todo = await prisma.todo.create({
 			data: {
 				name: name,
